68a981ef4cce72e28890f6d9c20b9099ec5bbc30
sd <superden125@gmail.comgit config --global user.name superden125>
2020-07-20 16:23:39 +0700

    Updates

---
+++ precache-manifest.6ff75717bd935d5e2ba286d26e41c099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0c1a2cd04da24594c2f0546d42e773",
+    "url": "/react-note/index.html"
+  },
+  {
+    "revision": "ffb7803636abba4b28ff",
+    "url": "/react-note/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7b2b83306b444bdd957e",
+    "url": "/react-note/static/css/main.b0a18940.chunk.css"
+  },
+  {
+    "revision": "ffb7803636abba4b28ff",
+    "url": "/react-note/static/js/2.3b5dade3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-note/static/js/2.3b5dade3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2b83306b444bdd957e",
+    "url": "/react-note/static/js/main.9bbab1f6.chunk.js"
+  },
+  {
+    "revision": "32018a9ca5e054e65ba1",
+    "url": "/react-note/static/js/runtime-main.95287896.js"
+  }
+]);
